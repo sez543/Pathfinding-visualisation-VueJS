d16b1a93add51e9adfe372b644067a77548a6685
sez543 <58361076+sez543@users.noreply.github.com>
2020-11-27 09:17:34 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<!--
+Legend: 'O' -> Origin; 'D' -> Destination; 'W' -> Wall;  'N' -> Weight (Where N is the weight amount)
+-->
+
+<template>
+  <div id="app">
+    <Navigation></Navigation>
+    <Board :padding="padding" :width="width"></Board>
+  </div>
+</template>
+
+<script>
+import Board from "./components/Board.vue";
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  name: "App",
+  components: {
+    Board,
+    Navigation,
+  },
+  data: function() {
+    return {
+      padding: 20,
+      width: 25,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<!-- Global Styles -->
+<style>
+html {
+  font-size: 32px;
+}
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Fira Code", monospace;
+}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- <nav>
+    <md-button class="md-raised md-primary">Default</md-button>
+  </nav> -->
+  <md-app>
+    <md-app-toolbar class="md-primary">
+      <md-field>
+        <label for="algorithms">Algorithms</label>
+        <md-select v-model="algorithms" name="algorithms" id="algorithms">
+          <md-option value="dijkstra">Dijkstra's Algorithm</md-option>
+          <md-option value="a*">A* Search</md-option>
+          <md-option value="floyd">Floyd's Algorithm</md-option>
+        </md-select>
+      </md-field>
+      <md-button class="md-raised md-accent">Start</md-button>
+      <md-field>
+        <label for="patterns">Mazes and Patterns</label>
+        <md-select v-model="patterns" name="patterns" id="patterns">
+          <md-option value="maze">Basic Random maze</md-option>
+          <md-option value="stair">Simple Stair Pattern</md-option>
+          <md-option value="reccursive">Reccursive Division</md-option>
+        </md-select>
+      </md-field>
+    </md-app-toolbar>
+  </md-app>
+</template>
+
+<script>
+export default {
+  name: "Board",
+  data: () => ({
+    algorithms: [],
+    patterns: [],
+  }),
+};
+</script>
+
+<style>
+.md-button .md-ripple {
+  padding: 10px !important;
+}
+</style>
+
+<style scoped>
+.md-app-toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 30px;
+}
+.md-button {
+  height: auto;
+  font-size: 1.5em;
+}
+.md-field {
+  max-width: 300px;
+}
+</style>
